5aded2842d31188051b936d9cddf056f27e48b91
Stuart Fleishman <stuart.a.fleishman@gmail.com>
2020-09-30 17:18:57 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,7 +1,8 @@
 class CashRegister 
   
-  def initialize
-    @cash_register_with_discount=20
+  attr_accessor :cash_register :cash_register_with_discount
+  
+  def initialize(cash_register_with_discount = nil)
     @total = 0 
   end 
 
